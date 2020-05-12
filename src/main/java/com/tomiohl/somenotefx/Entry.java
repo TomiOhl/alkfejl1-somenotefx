4bc40d8f6b12da85040ef8ca9e7463bd4e1f5141
Tomi_Ohl <ohlslager.tom@gmail.com>
2020-05-12 17:32:27 +0200

    Megjelenés javítva gtk-t használó rendszereken

--- src/main/java/com/tomiohl/somenotefx/Entry.java
+++ src/main/java/com/tomiohl/somenotefx/Entry.java
@@ -1,8 +1,12 @@
 package com.tomiohl.somenotefx;
 
-// ennek semmi értelme, csak a jar miatt kellett
+// Az alkalmazás belépési pontja, főleg a jar miatt kellett.
 public class Entry {
     public static void main(String[] args) {
+        /* A következő sor megoldja a javafx és a GTK kölcsönös problémáit.
+        Ebből adódóan nem minden platformon érvényes, ha hibát tapasztalsz, próbáld kikommentezni.*/
+        System.setProperty("jdk.gtk.version","2");
+        // Ez lejjebb pedig az alkalmazás indítása, nehogy kiszedd.
         App.main(args);
     }
 }
