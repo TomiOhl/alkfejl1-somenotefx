655d9144b52e768b615e885f0f11b8e989c053cc
Tomi_Ohl <ohlslager.tom@gmail.com>
2020-05-12 16:30:29 +0200

    Futtatható jar készítésének lehetősége

---
+++ src/main/java/com/tomiohl/somenotefx/Entry.java
@@ -0,0 +1,8 @@
+package com.tomiohl.somenotefx;
+
+// ennek semmi értelme, csak a jar miatt kellett
+public class Entry {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
